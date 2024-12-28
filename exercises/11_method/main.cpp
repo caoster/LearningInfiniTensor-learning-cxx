8898df77e2bd8fe7478b3416440f258d1914d2cd
Suting Chen <caoster@icloud.com>
2024-12-28 16:35:22 -0600

    11

--- exercises/11_method/main.cpp
+++ exercises/11_method/main.cpp
@@ -4,9 +4,9 @@ struct Fibonacci {
     unsigned long long cache[128];
     int cached;
 
-    // TODO: 实现正确的缓存优化斐波那契计算
+    // DONE: 实现正确的缓存优化斐波那契计算
     unsigned long long get(int i) {
-        for (; false; ++cached) {
+        for (; cached < 128; ++cached) {
             cache[cached] = cache[cached - 1] + cache[cached - 2];
         }
         return cache[i];
@@ -14,8 +14,11 @@ struct Fibonacci {
 };
 
 int main(int argc, char **argv) {
-    // TODO: 初始化缓存结构体，使计算正确
-    Fibonacci fib;
+    // DONE: 初始化缓存结构体，使计算正确
+    Fibonacci fib{
+        .cache = {0, 1},
+        .cached = 2,
+    };
     ASSERT(fib.get(10) == 55, "fibonacci(10) should be 55");
     std::cout << "fibonacci(10) = " << fib.get(10) << std::endl;
     return 0;
