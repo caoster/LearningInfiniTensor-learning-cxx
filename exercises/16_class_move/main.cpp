9f40d3f82b3aa8a8d226750d10ab78f17706aaf8
Suting Chen <caoster@icloud.com>
2024-12-28 21:07:25 -0600

    16

--- exercises/16_class_move/main.cpp
+++ exercises/16_class_move/main.cpp
@@ -14,22 +14,40 @@ class DynFibonacci {
     int cached;
 
 public:
-    // TODO: 实现动态设置容量的构造器
-    DynFibonacci(int capacity): cache(new ?), cached(?) {}
+    // DONE: 实现动态设置容量的构造器
+    explicit DynFibonacci(int capacity) : cache(new size_t[capacity]), cached(2) {
+        cache[0] = 0;
+        cache[1] = 1;
+    }
 
-    // TODO: 实现移动构造器
-    DynFibonacci(DynFibonacci &&) noexcept = delete;
+    // DONE: 实现移动构造器
+    DynFibonacci(DynFibonacci &&that) noexcept {
+        this->cached = that.cached;
+        this->cache = that.cache;
+        that.cache = nullptr;
+    }
 
-    // TODO: 实现移动赋值
+    // DONE: 实现移动赋值
     // NOTICE: ⚠ 注意移动到自身问题 ⚠
-    DynFibonacci &operator=(DynFibonacci &&) noexcept = delete;
+    DynFibonacci &operator=(DynFibonacci &&that) noexcept {
+        if (this != &that) {
+            delete[] cache;
+            cache = that.cache;
+            cached = that.cached;
+            that.cache = nullptr;
+            that.cached = 0;
+        }
+        return *this;
+    }
 
-    // TODO: 实现析构器，释放缓存空间
-    ~DynFibonacci();
+    // DONE: 实现析构器，释放缓存空间
+    ~DynFibonacci() {
+        delete[] cache;
+    }
 
-    // TODO: 实现正确的缓存优化斐波那契计算
+    // DONE: 实现正确的缓存优化斐波那契计算
     size_t operator[](int i) {
-        for (; false; ++cached) {
+        for (; cached <= i; ++cached) {
             cache[cached] = cache[cached - 1] + cache[cached - 2];
         }
         return cache[i];
