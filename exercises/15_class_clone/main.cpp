9d92aa0b326bed80e57e182c7f2bc6b1031d864c
Suting Chen <caoster@icloud.com>
2024-12-28 19:00:44 -0600

    15

--- exercises/15_class_clone/main.cpp
+++ exercises/15_class_clone/main.cpp
@@ -9,18 +9,29 @@ class DynFibonacci {
     int cached;
 
 public:
-    // TODO: 实现动态设置容量的构造器
-    DynFibonacci(int capacity): cache(new ?), cached(?) {}
+    // DONE: 实现动态设置容量的构造器
+    explicit DynFibonacci(int capacity): cache(new size_t[capacity]), cached(2) {
+        cache[0] = 0;
+        cache[1] = 1;
+    }
 
-    // TODO: 实现复制构造器
-    DynFibonacci(DynFibonacci const &) = delete;
+    // DONE: 实现复制构造器
+    DynFibonacci(DynFibonacci const & that) {
+        cache = new size_t[that.cached];
+        cached = that.cached;
+        for (int i = 0; i < cached; ++i) {
+            cache[i] = that.cache[i];
+        }
+    }
 
-    // TODO: 实现析构器，释放缓存空间
-    ~DynFibonacci();
+    // DONE: 实现析构器，释放缓存空间
+    ~DynFibonacci() {
+        delete[] cache;
+    }
 
-    // TODO: 实现正确的缓存优化斐波那契计算
+    // DONE: 实现正确的缓存优化斐波那契计算
     size_t get(int i) {
-        for (; false; ++cached) {
+        for (; cached <= i; ++cached) {
             cache[cached] = cache[cached - 1] + cache[cached - 2];
         }
         return cache[i];
